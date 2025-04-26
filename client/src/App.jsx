9f5fc3a5ae51fd6aa3cf89d734cd3e71eb73d142
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-04-26 19:52:02 +0100

    final registration view and basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Registration from "./pages/Registration";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <Registration />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import Login from "./Login";
 
 function Registration() {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const responce = await axios.post(`${API_URL}/auth/register`, {
+        name,
+        email,
+        password,
+      });
+      console.log(responce.data.message);
+      if (responce.data.success) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="min-h-screen bg-backgroundtheme flex justify-center items-center">
       <div className="bg-white rounded-2xl py-6 px-10">
         <h2 className="block text-center text-4xl font-bold mb-2">Welcome!</h2>
         <p className="block text-center text-xl mb-6">Create an account</p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-2">
             <label className="ml-3" htmlFor="username">
               Username
             </label>
             <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="w-full px-5 py-2 bg-inputtheme rounded-4xl"
               type="text"
               placeholder="Enter username"
@@ -23,6 +49,8 @@ function Registration() {
               Email
             </label>
             <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="w-full px-5 py-2 bg-inputtheme rounded-4xl"
               type="email"
               placeholder="Enter email"
@@ -34,6 +62,8 @@ function Registration() {
               Password
             </label>
             <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full px-5 py-2 bg-inputtheme rounded-4xl"
               type="password"
               placeholder="Enter password"
@@ -41,9 +71,20 @@ function Registration() {
             />
           </div>
           <div className="flex justify-center items-center">
-            <button className="mt-6 mb-6 py-3 px-20 text-white font-bold text-[15px] bg-gbutton rounded-xl">
+            <button
+              type="submit"
+              className="mt-6 mb-6 py-3 px-20 text-white font-bold text-[15px] bg-gbutton rounded-xl cursor-pointer"
+            >
               Continue
             </button>
+          </div>
+          <div className="flex justify-center items-center">
+            <p>
+              Already have an account?
+              <Link to="/login" className="text-blue-600">
+                Login
+              </Link>
+            </p>
           </div>
         </form>
       </div>
